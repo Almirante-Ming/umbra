93a28244cccf01636e1200b315d0852f97a2ed3b
V.Guilherme <v.guilherme.barreto@gmail.com>
2025-07-11 10:16:34 -0400

    feat: add LoginForm component and related UI elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button"
+import { LoginForm } from "./components/login"
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+      <LoginForm>
+
+      </LoginForm>
     </div>
   )
 }
